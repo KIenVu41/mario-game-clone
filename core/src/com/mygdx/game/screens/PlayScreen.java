42cc8e4e2f1225f4ee600d0301dd365f9c98e269
kienvt <kienvutrung20@gmail.com>
2022-11-15 16:34:02 +0700

    add score & time

--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -93,6 +93,7 @@ public class PlayScreen implements Screen {
         world.step(1/60f, 6, 2);
 
         player.update(dt);
+        hud.update(dt);
 
         gamecam.position.x = player.b2body.getPosition().x;
 
--- core/src/com/mygdx/game/sprites/Coin.java
+++ core/src/com/mygdx/game/sprites/Coin.java
@@ -1,22 +1,29 @@
 package com.mygdx.game.sprites;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.scenes.Hud;
 
 public class Coin extends InteractiveTileObject {
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_COIN = 28;
+
     public Coin(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
+        tileSet = map.getTileSets().getTileSet("tileset_gutter");
         fixture.setUserData(this);
         setCategoryFilter(MyGdxGame.COIN_BIT);
     }
 
     @Override
     public void onHeadHit() {
-
+        getCell().setTile(tileSet.getTile(BLANK_COIN));
+        Hud.addScore(100);
     }
 }
